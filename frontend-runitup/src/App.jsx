0b9d119087495d5fe839cdf41b20848cc4df2772
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-06-26 14:37:41 -0700

    added the routing to the App.jsx file. I also created a new folder and file called Main just to test out functionality later on.

--- frontend-runitup/src/App.jsx
+++ frontend-runitup/src/App.jsx
@@ -1,15 +1,40 @@
 import './App.css'
 import SignUp from './Components/Signup_Page/SignUp'
 import LoginPage from './Components/Login_Page/LoginPage'
-
+import { useState, useEffect } from "react";
+import { UserContext } from "./UserContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./components/Main/Main";
 
 function App() {
+  const [user, setUser] = useState(() => {
+    // Retrieve the user data from storage or set it to null if not found
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const updateUser = (newUser) => {
+    setUser(newUser);
+  };
+
+  useEffect(() => {
+    // Save the user data to storage whenever the user state changes
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
   return (
-    <>
-      <SignUp />
-      <LoginPage />
-    </>
-  )
+    <div className="app">
+      <UserContext.Provider value={{ user, updateUser }}>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/" element={<Main />} /> */}
+            <Route path="/" element={user ? <Main /> : <LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </div>
+  );
 }
 
 export default App
