450a7a1df2a43b15c7ac929b38c95d6b79d036fc
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-06-26 14:18:25 -0700

    added the functionality to the login page.

--- frontend-runitup/src/Components/Login_Page/LoginPage.jsx
+++ frontend-runitup/src/Components/Login_Page/LoginPage.jsx
@@ -1,25 +1,81 @@
 import "./LoginPage.css";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext.js";
+
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { updateUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make the login API request
+      const response = await fetch(`http://localhost:3000/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const loggedInUser = data.user;
+
+        // Update the user context
+        updateUser(loggedInUser);
+
+        // Navigate to the home page after successful login
+        navigate("/");
+      } else {
+        // Handle the login failure case
+        alert("Login failed");
+      }
+    } catch (error) {
+      // Handle any network or API request errors
+      alert("Login failed: " + error);
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="left-side">
         <div className="login-form">
           <h1>Login</h1>
-          <form>
+          <form onSubmit={handleLogin}>
             <label>
               Email
-              <input type="email" placeholder="Ex. jonas_kahrwaldi@gmail.com" />
+              <input
+                type="text"
+                placeholder="Ex. jonas_kahrwaldi@gmail.com"
+                id="email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
             </label>
             <label>
               Password
-              <input type="password" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </label>
             <button type="submit" className="login-button">
-              Log In
+              Login
             </button>
           </form>
           <p className="login-link">
-            Need an account <a href="/signup">Create one</a>
+            Need an account <Link to="/signup">Create one</Link>
           </p>
         </div>
       </div>
