be303a11e1d0292d4eae5dc5ac609a62565f2652
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-06-26 14:25:33 -0700

    added the functionality for the sign up page. Also i chanfed the full name to just username since some users might not feel comfortable writing thier full name.

--- frontend-runitup/src/Components/Signup_Page/SignUp.jsx
+++ frontend-runitup/src/Components/Signup_Page/SignUp.jsx
@@ -1,31 +1,101 @@
 import "./SignUp.css";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext.js";
 
 function SignUp() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { updateUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make the signup API request
+      const response = await fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const loggedInUser = data.user;
+
+        console.log("Signup successful");
+
+        // Reset form fields
+        setUsername("");
+        setEmail("");
+        setPassword("");
+
+        // Update the user context
+        updateUser(loggedInUser);
+
+        // Navigate to the home page after successful login
+        navigate("/");
+      } else {
+        // Handle signup failure case
+        alert("Signup failed");
+      }
+    } catch (error) {
+      // Handle any network or API request errors
+      alert("Signup failed: " + error);
+    }
+  };
+
   return (
     <div className="signup-container">
       <div className="left-side">
         <div className="signup-form">
           <h1>Sign up</h1>
           <p>Sign up to enjoy the features of Run It Up</p>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>
-              Full Name
-              <input type="text" placeholder="Ex. Jonas Khurwaldi" />
+              Username
+              <input
+                type="text"
+                placeholder="Ex. J_Khurwaldi"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
             </label>
             <label>
               Email
-              <input type="email" placeholder="Ex. jonas_kahrwaldi@gmail.com" />
+              <input
+                type="email"
+                placeholder="Ex. jonas_kahrwaldi@gmail.com"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </label>
             <label>
               Password
-              <input type="password" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </label>
             <button type="submit" className="signup-button">
               Sign up
             </button>
           </form>
           <p className="login-link">
-            Already have an account? <a href="/login">Sign in</a>
+            Already have an account? <Link to="/login">Sign in</Link>
           </p>
         </div>
       </div>
