0b384746034ff6712105a1f6c94c4592b2b0bc50
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-06-30 21:19:49 -0700

    added a component for the navbar so i can switch between the home and community page.

--- frontend-runitup/src/Components/Feed/FeedPage.jsx
+++ frontend-runitup/src/Components/Feed/FeedPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../utils/Header";
 import Footer from "../../utils/Footer";
+import NavBar from "./NavBar";
 function Feed({ user, onLogout }) {
   return (
     <div className="main-container">
       <Header onLogout={onLogout} />
+      <NavBar/>
       <h1>Welcome to Feeds Page!</h1>
       <h1>{user.name}</h1>
       <Footer />
---
+++ frontend-runitup/src/Components/Feed/NavBar.jsx
@@ -0,0 +1,14 @@
+import "../../styles/NavBar.css"
+
+function NavBar() {
+  return (
+    <div className="navbar-container">
+      <nav>
+        <a href="/">HOME</a>
+        <a href="/community">COMMUNITY</a>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
