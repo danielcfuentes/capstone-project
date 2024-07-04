0b58f819d49bae51320888232a521a9eb5139016
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-07-03 17:37:57 -0700

    updated schema in order to handle bytes for image upload and ran the migration.

---
+++ backend/prisma/migrations/20240704003658_updated_imagetable/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `url` on the `Image` table. All the data in the column will be lost.
+  - Added the required column `data` to the `Image` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `mimeType` to the `Image` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Image" DROP COLUMN "url",
+ADD COLUMN     "data" BYTEA NOT NULL,
+ADD COLUMN     "mimeType" TEXT NOT NULL;
