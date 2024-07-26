862ed1be53c0bccccc8c769b3e45296b2d5b6134
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-07-26 09:01:05 -0700

    created a function t create a graph based on the road network.

--- backend/routeGenerator.js
+++ backend/routeGenerator.js
@@ -17,3 +17,41 @@ async function fetchRoadNetwork(startLat, startLng, radiusKm) {
   );
   return response.data;
 }
+
+
+function createGraph(osmData) {
+  const graph = new Map();
+
+  osmData.elements.forEach((element) => {
+    if (element.type === "node") {
+      graph.set(element.id, {
+        id: element.id,
+        lat: element.lat,
+        lon: element.lon,
+        connections: new Set(),
+      });
+    }
+  });
+
+  osmData.elements.forEach((element) => {
+    if (
+      element.type === "way" &&
+      element.tags &&
+      (element.tags.highway || element.tags.footway)
+    ) {
+      for (let i = 0; i < element.nodes.length - 1; i++) {
+        const node1 = graph.get(element.nodes[i]);
+        const node2 = graph.get(element.nodes[i + 1]);
+
+        if (node1 && node2) {
+          node1.connections.add(node2.id);
+          node2.connections.add(node1.id);
+        }
+      }
+    }
+  });
+
+  return graph;
+}
+
+
