c4cdf54d49a790f9a3e577e58453c56e68d43882
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-07-26 21:20:58 -0700

    added schema for particpating events.

---
+++ backend/prisma/migrations/20240727041903_added_particpate_events/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_EventParticipants" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_EventParticipants_AB_unique" ON "_EventParticipants"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_EventParticipants_B_index" ON "_EventParticipants"("B");
+
+-- AddForeignKey
+ALTER TABLE "_EventParticipants" ADD CONSTRAINT "_EventParticipants_A_fkey" FOREIGN KEY ("A") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EventParticipants" ADD CONSTRAINT "_EventParticipants_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
